c9526f53c5b69c4c92bdaac2526b423e6a0df80b
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-02-21 18:25:03 -0300

    Agregando componente LINK

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const header = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-conten-between">
             <div className="container">
-                <h1>CRUD CON REACT, REDUX, REST API & AXIOS</h1>
+                <h1>
+                    <Link to={'/'} className="text-light" >CRUD CON REACT, REDUX, REST API & AXIOS</Link>
+                </h1>
             </div>
 
-            <a href="/productos/nuevo" className="btn btn-danger nuevo-post d-block d-md-inline-block"
-            >Agregar Producto &#43;</a>
+            <Link to={'/productos/nuevo'} className="btn btn-danger nuevo-post d-block d-md-inline-block"
+            >Agregar Producto &#43;</Link>
         </nav>
      );
 }
