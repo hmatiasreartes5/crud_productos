a0e9e39c416f6175b438d507f494c7bb9284857e
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-02-27 23:37:20 -0300

    Agregando funcionalidad para insertar productos en la API

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,10 +3,39 @@ import {
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR
 } from '../types/index';
+import clienteAxios from '../config/axios'
+
 
 //Crear nuevo producto
 export function crearNuevoProductoAction (producto){
-    return() =>{
-        console.log(producto)
+    return async(dispatch) =>{
+        dispatch(agregarProducto());
+        try {
+            //insertar en la API
+            await clienteAxios.post('/productos',producto)
+            
+            //si todo sale bien actualizar el state
+            dispatch(agregarProductoExito(producto));
+        } catch (error) {
+            console.log(error)
+            //si hay un error cambiar el state
+            dispatch(agregarProductoError(true));
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO
+})
+
+//almacena el producto con exito
+const agregarProductoExito = producto => ({ //esto que esta entre parentesis es el action del reducer!!!!
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+//Error al almacenar el producto
+const agregarProductoError = estado => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
